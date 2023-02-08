b3d9a6e9d8f37a99f5320cd3fbee69b9161bd147
Alexandr-Kuzmin13 <109796811+Alexandr-Kuzmin13@users.noreply.github.com>
2023-02-08 08:37:58 +0300

    Update MigrationGenerator.java

--- app/src/main/java/hexlet/code/MigrationGenerator.java
+++ app/src/main/java/hexlet/code/MigrationGenerator.java
@@ -11,7 +11,7 @@ public final class MigrationGenerator {
 
         DbMigration dbMigration = DbMigration.create();
 
-        //dbMigration.addPlatform(Platform.POSTGRES, "postgres");
+        dbMigration.addPlatform(Platform.POSTGRES, "postgres");
         dbMigration.addPlatform(Platform.H2, "h2");
 
         dbMigration.generateMigration();

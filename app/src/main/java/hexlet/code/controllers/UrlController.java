2e74f0534f8b7c517ed111e4cb956ac6311f7d9b
Alexandr-Kuzmin13 <109796811+Alexandr-Kuzmin13@users.noreply.github.com>
2023-02-09 12:00:58 +0300

    Update UrlController.java

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -70,7 +70,7 @@ public class UrlController {
     };
 
     public static Handler createUrl = ctx -> {
-        String name = ctx.formParam("url");
+        String name = ctx.formParam("name");
 
         String newName = "";
 

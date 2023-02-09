6bd1ba66412ab3358005faf4bc4738d30fee919d
Alexandr-Kuzmin13 <109796811+Alexandr-Kuzmin13@users.noreply.github.com>
2023-02-09 12:32:28 +0300

    Update UrlController.java

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -72,10 +72,6 @@ public class UrlController {
     public static Handler createUrl = ctx -> {
         String name = ctx.formParam("name");
 
-        if (name == null) {
-            throw new NotFoundResponse();
-        }
-
         String newName = "";
 
         try {
